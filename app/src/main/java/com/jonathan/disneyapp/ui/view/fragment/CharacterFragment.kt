2dcaf54393a866e604023d88f2c7356c731527e6
Jonathan Javier Ramirez <jonathanramirez@wposs.com>
2022-07-08 11:29:42 -0500

    Implementación de NavigationComponent en el flujo de home

---
+++ app/src/main/java/com/jonathan/disneyapp/ui/view/fragment/CharacterFragment.kt
@@ -0,0 +1,18 @@
+package com.jonathan.disneyapp.ui.view.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.jonathan.disneyapp.databinding.FragmentCharacterBinding
+
+class CharacterFragment : Fragment() {
+
+    private lateinit var binding: FragmentCharacterBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentCharacterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
