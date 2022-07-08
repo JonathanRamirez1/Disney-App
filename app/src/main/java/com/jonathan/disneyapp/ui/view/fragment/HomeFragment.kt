d008679aea1e95772b663712a2f5df9915c46969
Jonathan Javier Ramirez <jonathanramirez@wposs.com>
2022-07-08 13:52:59 -0500

    Cración del diseño de Home View

--- app/src/main/java/com/jonathan/disneyapp/ui/view/fragment/HomeFragment.kt
+++ app/src/main/java/com/jonathan/disneyapp/ui/view/fragment/HomeFragment.kt
@@ -5,15 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.jonathan.disneyapp.R
 import com.jonathan.disneyapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
+    private lateinit var navController: NavController
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        launchMovieFragment(view)
+        launchCharacterFragment(view)
+    }
+
+    private fun launchMovieFragment(view: View) {
+        val movie = binding.cardViewMovies
+
+        navController = Navigation.findNavController(view)
+        movie.setOnClickListener {
+            navController.navigate(R.id.movieFragment)
+        }
+    }
+
+    private fun launchCharacterFragment(view: View) {
+        val character = binding.cardViewCharacters
+
+        navController = Navigation.findNavController(view)
+        character.setOnClickListener {
+            navController.navigate(R.id.characterFragment)
+        }
     }
 }
